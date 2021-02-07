d98bb6ceafb4335d0ef334d3e81da90127307226
LM7 <legitmichel777@outlook.com>
2021-02-07 09:53:12 +0800

    Didn't commit 3 files for some reason8

---
+++ CoreOrate/lltfunc.cpp
@@ -0,0 +1,8 @@
+//
+//  lltfunc.cpp
+//  CoreOrate
+//
+//  Created by LegitMichel777 on 2021/2/7.
+//
+
+#include "lltfunc.hpp"
---
+++ CoreOrate/lltfunc.hpp
@@ -0,0 +1,14 @@
+//
+//  lltfunc.hpp
+//  CoreOrate
+//
+//  Created by LegitMichel777 on 2021/2/7.
+//
+
+#ifndef lltfunc_hpp
+#define lltfunc_hpp
+
+#include <stdio.h>
+
+
+#endif /* lltfunc_hpp */
